4125202e4296a31d9f3372e11ba24ea03117f1f1
anasevich <andrey.nasevich@idt.net>
2023-04-16 14:46:16 +0200

    Add a base Angular project

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "main1",
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
+})
+export class MainComponent{
+    
+}
